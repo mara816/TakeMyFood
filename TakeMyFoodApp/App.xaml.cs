85773d35944ef32c704e6b785b21fc3164de7535
Mathias Rasmussen <mteu@interacoustics.com>
2023-05-10 09:25:17 +0200

    Initial commit

---
+++ TakeMyFoodApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace TakeMyFoodApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ TakeMyFoodApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace TakeMyFoodApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
